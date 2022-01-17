07c692c474376e89d245a6b88d61592b7f9eb66c
Wayde Gilliam <waydegilliam@gmail.com>
2022-01-17 14:25:50 -0800

    updated colorscheme variables

--- nvim/.config/nvim/lua/waydegg/colorscheme.lua
+++ nvim/.config/nvim/lua/waydegg/colorscheme.lua
@@ -1,6 +1,8 @@
+vim.g.neosolarized_contrast = "high"
+vim.g.neosolarized_termtrans = 1
+
 local status_ok, _ = pcall(vim.cmd, "colorscheme NeoSolarized") 
 if not status_ok then
   print("Unable to load NeoSolarized")
   return
 end
-vim.g.neosolarized_contrast = "high"
