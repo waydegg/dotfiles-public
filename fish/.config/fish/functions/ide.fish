19e5ba62cab6de8d45e9251fe889909e806033dd
Wayde Gilliam <waydegilliam@gmail.com>
2022-01-20 21:29:19 -0800

    updated ide func

--- fish/.config/fish/functions/ide.fish
+++ fish/.config/fish/functions/ide.fish
@@ -1,7 +1,18 @@
 function ide
-  tmux split-window -v -p 30
-  tmux split-window -h -p 66
-  tmux split-window -h -p 50
-  tmux select-pane -U
-  nvim
+  if set -q $TMUX
+    set tmux_session_id (tmux new-session -d -P nvim)
+    tmux split-window -t $tmux_session_id -v -p 30
+    tmux split-window -t $tmux_session_id -h -p 66
+    tmux split-window -t $tmux_session_id -h -p 50
+    tmux select-pane -U -t $tmux_session_id
+    tmux attach -t $tmux_session_id
+  else
+    tmux split-window  -v -p 30
+    tmux split-window  -h -p 66
+    tmux split-window  -h -p 50
+    tmux select-pane -U 
+    nvim
+  end
 end
+
+
