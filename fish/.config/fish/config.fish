c0caf1e8dba645674216b1773b6cc00536522ac1
Wayde Gilliam <waydegilliam@gmail.com>
2022-04-10 13:17:26 -0700

    bat diff and cleaner ide function

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -28,7 +28,7 @@ alias tree "tree -l -C -a"
 alias g git
 alias gs "git status"
 alias ga "git add"
-alias gd "git diff"
+alias gd "git diff --name-only --diff-filter=d | xargs bat --diff"
 alias gc "git commit -m"
 
 alias t tmux
@@ -69,20 +69,11 @@ end
 
 # IDE-like window splits
 function ide
-  if set -q $TMUX
-    set tmux_session_id (tmux new-session -d -P nvim)
-    tmux split-window -t $tmux_session_id -v -p 30
-    tmux split-window -t $tmux_session_id -h -p 66
-    tmux split-window -t $tmux_session_id -h -p 50
-    tmux select-pane -U -t $tmux_session_id
-    tmux attach -t $tmux_session_id
-  else
-    tmux split-window -v -p 30
-    tmux split-window -h -p 66
-    tmux split-window -h -p 50
-    tmux select-pane -U 
-    nvim
-  end
+  tmux split-window -v -p 30
+  tmux split-window -h -p 66
+  tmux split-window -h -p 50
+  tmux select-pane -U 
+  nvim
 end
 
 # ===== Misc ==================================================================
