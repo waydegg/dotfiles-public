b65b9f4ab9698c1651d7a9b552bc9d27b8aee3d9
Wayde Gilliam <waydegilliam@gmail.com>
2022-11-14 00:13:55 -0800

    gh install

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -103,6 +103,14 @@ wget https://github.com/antonmedv/fx/releases/download/24.0.0/fx_linux_amd64 -P 
 sudo cp Downloads/fx_linux_amd64 /usr/local/bin/fx
 sudo chmod +x /usr/local/bin/fx
 
+# Install gh
+type -p curl >/dev/null || sudo apt install curl -y
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+&& sudo apt update \
+&& sudo apt install gh -y
+
 # Delete all files in /Downloads
 rm -r ~/Downloads/*
 
