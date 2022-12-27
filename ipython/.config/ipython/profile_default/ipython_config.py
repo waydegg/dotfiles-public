8461064d0cbe9cec28571e3f3619fb9b0aea6de9
Wayde Gilliam <waydegilliam@gmail.com>
2022-12-26 18:16:05 -0800

    add some shell aliases

--- ipython/.config/ipython/profile_default/ipython_config.py
+++ ipython/.config/ipython/profile_default/ipython_config.py
@@ -1,8 +1,12 @@
 c = get_config()
 
 c.InteractiveShell.autoindent = False
-
 c.InteractiveShellApp.extensions = ["autoreload"]
 c.InteractiveShellApp.exec_lines = ["%autoreload 2"]
-
 c.TerminalInteractiveShell.editing_mode = "vi"
+c.AliasManager.user_aliases = [
+    ("c", "clear"),
+    ("la", "la"),
+    ("ll", "ll"),
+    ("lla", "lla"),
+]
