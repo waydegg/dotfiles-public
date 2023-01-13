bbd477ae784aeeae95f3409c9bbfcb859d96c278
Wayde Gilliam <waydegilliam@gmail.com>
2023-01-12 21:36:37 -0800

    update fish ide func

--- fish/.config/fish/functions/ide.fish
+++ fish/.config/fish/functions/ide.fish
@@ -1,12 +1,26 @@
-function ide -d "IDE-like window splits"
+function ide --description "IDE-like window splits" --argument n_panes
   if not set -q TMUX
     echo "No tmux session attached"
     return 1
   end
-
+  
+  if not string match --quiet --regex --invert '\D' $n_panes
+    echo "Number of panes must be an integer"
+    return 1
+  else if test -z $n_panes
+    set n_panes 3
+  else if test $n_panes -le 1 
+    echo "Number of panes must be greater than 1"
+    return 1
+  end
+  
   tmux split-window -v -p 30
-  tmux split-window -h -p 66
-  tmux split-window -h -p 50
+
+  for i in (seq $n_panes 2)
+    set split_p (math floor 100 - 100 / $i)
+    tmux split-window -h -p $split_p
+  end
+
   tmux select-pane -U
   nvim
 end
