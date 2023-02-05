995c96ba2573d78b19305efc7c317f7f7f7aecbc
Wayde Gilliam <waydegilliam@gmail.com>
2023-02-04 16:12:29 -0800

    add auto-import for pyopp pretty_print

--- ipython/.config/ipython/profile_default/ipython_config.py
+++ ipython/.config/ipython/profile_default/ipython_config.py
@@ -3,7 +3,15 @@ c = get_config()  # type: ignore
 c.TerminalIPythonApp.display_banner = False
 c.InteractiveShell.autoindent = False
 c.InteractiveShellApp.extensions = ["autoreload"]
-c.InteractiveShellApp.exec_lines = ["%autoreload 2"]
+c.InteractiveShellApp.exec_lines = [
+    "%autoreload 2",
+    """
+    try:
+        from pyopp import pretty_print as pp
+    except:
+        pass
+    """,
+]
 c.TerminalInteractiveShell.editing_mode = "vi"
 c.AliasManager.user_aliases = [
     ("c", "clear"),
