297b0cff97775e01a2cd56ba4d899c07686b9391
Wayde Gilliam <waydegilliam@gmail.com>
2023-10-25 18:05:41 -0700

    add ruff lsp

--- nvim/.config/nvim/lua/config/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/config/plugins/lsp-config.lua
@@ -1,47 +1,47 @@
 local lspconfig_ok, lspconfig = pcall(require, "lspconfig")
 if not lspconfig_ok then
-	print("'lspconfig' not installed")
-	return
+  print("'lspconfig' not installed")
+  return
 end
 
 local typescript_ok, typescript = pcall(require, "typescript")
 if not typescript_ok then
-	print("'typescript' not installed")
-	return
+  print("'typescript' not installed")
+  return
 end
 
 -- Diagnostic signs
 
 local signs = {
-	{ name = "DiagnosticSignError", text = "" },
-	{ name = "DiagnosticSignWarn", text = "" },
-	{ name = "DiagnosticSignHint", text = "" },
-	{ name = "DiagnosticSignInfo", text = "" },
+  { name = "DiagnosticSignError", text = "" },
+  { name = "DiagnosticSignWarn",  text = "" },
+  { name = "DiagnosticSignHint",  text = "" },
+  { name = "DiagnosticSignInfo",  text = "" },
 }
 for _, sign in ipairs(signs) do
-	vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
+  vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
 end
 
 vim.diagnostic.config({
-	virtual_text = true,
-	signs = {
-		active = signs,
-	},
-	severity_sort = true,
-	float = {
-		border = true,
-	},
+  virtual_text = true,
+  signs = {
+    active = signs,
+  },
+  severity_sort = true,
+  float = {
+    border = true,
+  },
 })
 
 -- Hover doc & signature handlers
 
 local border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" }
 local handlers = {
-	["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = border }),
-	["textDocument/signatureHelp"] = vim.lsp.with(
-		vim.lsp.handlers.signature_help,
-		{ border = border }
-	),
+      ["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = border }),
+      ["textDocument/signatureHelp"] = vim.lsp.with(
+    vim.lsp.handlers.signature_help,
+    { border = border }
+  ),
 }
 
 -- Capabilities
@@ -49,8 +49,8 @@ local handlers = {
 -- Used for UFO
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.foldingRange = {
-	dynamicRegistration = false,
-	lineFoldingOnly = true,
+  dynamicRegistration = false,
+  lineFoldingOnly = true,
 }
 
 -- Used for CMP?
@@ -59,89 +59,105 @@ capabilities.textDocument.completion.completionItem.snippetSupport = true
 -- LSP config
 
 lspconfig.pyright.setup({
-	capabilities = capabilities,
-	filetypes = { "python" },
-	handlers = handlers,
+  capabilities = capabilities,
+  filetypes = { "python" },
+  handlers = handlers,
 })
 
 typescript.setup({
-	server = {
-		capabilities = capabilities,
-		filetypes = {
-			"typescript",
-			"typescriptreact",
-			"javascript",
-			"javascriptreact",
-		},
-		on_attach = function(client)
-			client.server_capabilities.document_formatting = false
-		end,
-	},
+  server = {
+    capabilities = capabilities,
+    filetypes = {
+      "typescript",
+      "typescriptreact",
+      "javascript",
+      "javascriptreact",
+    },
+    on_attach = function(client)
+      client.server_capabilities.document_formatting = false
+    end,
+  },
 })
 
 lspconfig.vimls.setup({
-	capabilities = capabilities,
-	filetypes = { "vim" },
+  capabilities = capabilities,
+  filetypes = { "vim" },
 })
 
 lspconfig.lua_ls.setup({
-	capabilities = capabilities,
-	filetypes = { "lua" },
-	settings = {
-		Lua = {
-			runtime = {
-				version = "LuaJIT",
-			},
-			diagnostics = {
-				globals = { "vim" },
-			},
-			workspace = {
-				library = vim.api.nvim_get_runtime_file("", true),
-			},
-			telemetry = {
-				enable = false,
-			},
-		},
-	},
-	on_attach = function(client)
-		client.server_capabilities.document_formatting = false
-	end,
+  capabilities = capabilities,
+  filetypes = { "lua" },
+  settings = {
+    Lua = {
+      runtime = {
+        version = "LuaJIT",
+      },
+      diagnostics = {
+        globals = { "vim" },
+      },
+      workspace = {
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+  on_attach = function(client)
+    client.server_capabilities.document_formatting = false
+  end,
 })
 
 lspconfig.rust_analyzer.setup({
-	capabilities = capabilities,
-	filetypes = { "rust" },
+  capabilities = capabilities,
+  filetypes = { "rust" },
 })
 
 lspconfig.html.setup({
-	capabilities = capabilities,
-	filetypes = { "html" },
-	init_options = {
-		configurationSection = { "html", "css", "javascript" },
-		embeddedLanguages = {
-			css = true,
-			javascript = true,
-		},
-	},
-	on_attach = function(client)
-		client.server_capabilities.document_formatting = false
-	end,
+  capabilities = capabilities,
+  filetypes = { "html" },
+  init_options = {
+    configurationSection = { "html", "css", "javascript" },
+    embeddedLanguages = {
+      css = true,
+      javascript = true,
+    },
+  },
+  on_attach = function(client)
+    client.server_capabilities.document_formatting = false
+  end,
 })
 
 lspconfig.cssls.setup({
-	capabilities = capabilities,
-	filetypes = { "css", "scss", "less" },
+  capabilities = capabilities,
+  filetypes = { "css", "scss", "less" },
 })
 
 lspconfig.vuels.setup({
-	capabilities = capabilities,
-	filetypes = { "vue" },
+  capabilities = capabilities,
+  filetypes = { "vue" },
 })
 
 lspconfig.tailwindcss.setup({
-	capabilities = capabilities,
+  capabilities = capabilities,
 })
 
 lspconfig.svelte.setup({
-	capabilities = capabilities,
+  capabilities = capabilities,
+})
+
+lspconfig.ruff_lsp.setup({
+  cmd = { vim.fn.stdpath("config") .. "/venv/bin/ruff-lsp" },
+  capabilities = capabilities,
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.lsp.buf.format({ bufnr = bufnr })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = bufnr })
+        end,
+      })
+    end
+  end,
 })
--- nvim/.config/nvim/lua/config/plugins/null-ls.lua
+++ nvim/.config/nvim/lua/config/plugins/null-ls.lua
@@ -1,76 +1,57 @@
 local ok, null_ls = pcall(require, "null-ls")
 if not ok then
-	print("'null-ls' not installed")
-	return
+  print("'null-ls' not installed")
+  return
 end
 
-local helpers = require("null-ls.helpers")
 local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
-local methods = null_ls.methods
-
-local autoflake = {
-	name = "autoflake",
-	method = methods.FORMATTING,
-	filetypes = { "python" },
-	generator = helpers.formatter_factory({
-		command = "autoflake",
-		args = {
-			"--remove-all-unused-imports",
-			"--stdout",
-			"$FILENAME",
-		},
-	}),
-}
 
 local prettier = formatting.prettier.with({
-	extra_filetypes = { "yml" },
-	args = function(params)
-		local args = {
-			"--config",
-			vim.fn.getenv("HOME") .. "/.config/prettier/prettier.config.js",
-			"--stdin-filepath",
-			"$FILENAME",
-		}
+  extra_filetypes = { "yml" },
+  args = function(_)
+    local args = {
+      "--config",
+      vim.fn.getenv("HOME") .. "/.config/prettier/prettier.config.js",
+      "--stdin-filepath",
+      "$FILENAME",
+    }
 
-		if vim.o.ft == "markdown" then
-			args = {
-				"--prose-wrap",
-				"always",
-				"--print-width",
-				"88",
-				"--stdin-filepath",
-				"$FILENAME",
-			}
-		end
+    if vim.o.ft == "markdown" then
+      args = {
+        "--prose-wrap",
+        "always",
+        "--print-width",
+        "88",
+        "--stdin-filepath",
+        "$FILENAME",
+      }
+    end
 
-		return args
-	end,
+    return args
+  end,
 })
 
 null_ls.setup({
-	sources = {
-		-- autoflake,
-		formatting.black,
-		formatting.isort,
-		prettier,
-		formatting.stylua,
-		formatting.taplo,
-		diagnostics.fish,
-		formatting.xmllint,
-		formatting.sql_formatter,
-	},
-	on_attach = function(client, bufnr)
-		-- Format on save
-		if client.supports_method("textDocument/formatting") then
-			vim.api.nvim_clear_autocmds({ group = "LspFormatting", buffer = bufnr })
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				group = "LspFormatting",
-				buffer = bufnr,
-				callback = function()
-					vim.lsp.buf.format({ bufnr = bufnr })
-				end,
-			})
-		end
-	end,
+  sources = {
+    prettier,
+    formatting.stylua,
+    formatting.taplo,
+    diagnostics.fish,
+    formatting.xmllint,
+    formatting.sql_formatter,
+  },
+  on_attach = function(client, bufnr)
+    -- Format on save
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({ group = "LspFormatting", buffer = bufnr })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = "LspFormatting",
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = bufnr })
+        end,
+      })
+    end
+  end,
 })
