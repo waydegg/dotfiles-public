bb9ec97b9f541f5441aad2de36e954e8f71e4242
Wayde Gilliam <waydegilliam@gmail.com>
2023-11-09 23:14:49 -0800

    update nvim-tree keymaps
    need to be put in an `on_attach` function

--- nvim/.config/nvim/lua/config/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/config/plugins/nvim-tree.lua
@@ -4,30 +4,41 @@ if not ok then
 	return
 end
 
+local function on_attach(bufnr)
+	local api = require("nvim-tree.api")
+
+	local function opts(desc)
+		return {
+			desc = "nvim-tree: " .. desc,
+			buffer = bufnr,
+			noremap = true,
+			silent = true,
+			nowait = true,
+		}
+	end
+
+	vim.keymap.set("n", "l", api.node.open.edit, opts("Open"))
+	vim.keymap.set("n", "<cr>", api.node.open.edit, opts("Open"))
+	vim.keymap.set("n", "h", api.node.navigate.parent_close, opts("Close Directory"))
+	vim.keymap.set("n", "r", api.fs.rename, opts("Rename"))
+	vim.keymap.set("n", "d", api.fs.remove, opts("Remove"))
+	vim.keymap.set("n", "c", api.tree.change_root_to_node, opts("CD"))
+	vim.keymap.set("n", "C", api.tree.change_root_to_parent, opts("Up"))
+	vim.keymap.set("n", "v", api.node.open.vertical, opts("Open: Vertical Split"))
+	vim.keymap.set("n", "b", api.node.open.horizontal, opts("Open: Horizontal Split"))
+	vim.keymap.set("n", "a", api.fs.create, opts("Create"))
+	vim.keymap.set("n", "<c-r>", api.tree.reload, opts("Reload"))
+	vim.keymap.set("n", "x", api.fs.cut, opts("Cut"))
+	vim.keymap.set("n", "p", api.fs.paste, opts("Paste"))
+end
+
 nvim_tree.setup({
+	on_attach = on_attach,
 	update_focused_file = {
 		enable = true,
 	},
 	view = {
 		width = 40,
-		mappings = {
-			custom_only = true,
-			list = {
-				{ key = "l", action = "edit" },
-				{ key = "<cr>", action = "edit" },
-				{ key = "h", action = "close_node" },
-				{ key = "r", action = "rename" },
-				{ key = "d", action = "remove" },
-				{ key = "c", action = "cd" },
-				{ key = "C", action = "dir_up" },
-				{ key = "v", action = "vsplit" },
-				{ key = "b", action = "split" },
-				{ key = "a", action = "create" },
-				{ key = "<c-r>", action = "refresh" },
-				{ key = "x", action = "cut" },
-				{ key = "p", action = "paste" },
-			},
-		},
 	},
 	renderer = {
 		group_empty = true,
