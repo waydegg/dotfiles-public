3ca848b8150e8853274e8d7942eef05cd3ec5285
Wayde Gilliam <waydegilliam@gmail.com>
2023-11-09 21:28:00 -0800

    add ruff lsp, format lua files

--- nvim/.config/nvim/lua/config/plugins/null-ls.lua
+++ nvim/.config/nvim/lua/config/plugins/null-ls.lua
@@ -1,57 +1,58 @@
 local ok, null_ls = pcall(require, "null-ls")
 if not ok then
-  print("'null-ls' not installed")
-  return
+	print("'null-ls' not installed")
+	return
 end
 
 local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 
 local prettier = formatting.prettier.with({
-  extra_filetypes = { "yml" },
-  args = function(_)
-    local args = {
-      "--config",
-      vim.fn.getenv("HOME") .. "/.config/prettier/prettier.config.js",
-      "--stdin-filepath",
-      "$FILENAME",
-    }
+	extra_filetypes = { "yml" },
+	args = function(_)
+		local args = {
+			"--config",
+			vim.fn.getenv("HOME") .. "/.config/prettier/prettier.config.js",
+			"--stdin-filepath",
+			"$FILENAME",
+		}
 
-    if vim.o.ft == "markdown" then
-      args = {
-        "--prose-wrap",
-        "always",
-        "--print-width",
-        "88",
-        "--stdin-filepath",
-        "$FILENAME",
-      }
-    end
+		if vim.o.ft == "markdown" then
+			args = {
+				"--prose-wrap",
+				"always",
+				"--print-width",
+				"88",
+				"--stdin-filepath",
+				"$FILENAME",
+			}
+		end
 
-    return args
-  end,
+		return args
+	end,
 })
 
 null_ls.setup({
-  sources = {
-    prettier,
-    formatting.stylua,
-    formatting.taplo,
-    diagnostics.fish,
-    formatting.xmllint,
-    formatting.sql_formatter,
-  },
-  on_attach = function(client, bufnr)
-    -- Format on save
-    if client.supports_method("textDocument/formatting") then
-      vim.api.nvim_clear_autocmds({ group = "LspFormatting", buffer = bufnr })
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        group = "LspFormatting",
-        buffer = bufnr,
-        callback = function()
-          vim.lsp.buf.format({ bufnr = bufnr })
-        end,
-      })
-    end
-  end,
+	sources = {
+		prettier,
+		formatting.isort,
+		formatting.stylua,
+		formatting.taplo,
+		diagnostics.fish,
+		formatting.xmllint,
+		formatting.sql_formatter,
+	},
+	on_attach = function(client, bufnr)
+		-- Format on save
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = "LspFormatting", buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = "LspFormatting",
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.format({ bufnr = bufnr })
+				end,
+			})
+		end
+	end,
 })
